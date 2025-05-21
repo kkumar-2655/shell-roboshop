3d323c0bf939f98670b2110f72c730e774a0fda6
Kiran <kirann.bobby@gmail.com>
2025-05-21 14:16:24 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME="daws84skiran.store"
 
 for instance in $@ 
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
